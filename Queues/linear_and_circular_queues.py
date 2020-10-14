123389182194f81d081c7c297d46cd0d79ec49d8
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2020-10-14 12:08:06 +0100

    Update linear_and_circular_queues.py

--- Queues/linear_and_circular_queues.py
+++ Queues/linear_and_circular_queues.py
@@ -27,19 +27,21 @@ class linearQueue():
             self.rear+=1
             self.queue.append(item)
             if len(self.queue) == self.maxSize:
-                print("The list can no longer enQueue from now as its maximum size has been reached.")
+                print("From now on, you can no longer enQueue to this queue as it has reached its maximum size.")
         else: print("Unable to enQueue. The queue is full.")
 
         
 
     def deQueue(self):
         if self.isEmpty() == False:
+            value = self.queue[self.front]
             self.front +=1
             if self.front > self.rear:
                 self.queueEmptyIndex = self.front
                 self.rear = None
                 self.front = None
-        else: print("Unable to deQueue. The queue is empty.")
+            return value
+        else: return "Unable to deQueue. The queue is empty."
 
 
     def showQueue(self):
@@ -47,6 +49,7 @@ class linearQueue():
             for i in range(self.queueSize()):
                 print(self.queue[self.front + i])
         else: print("Queue Is Empty.")
+        return ''
 
     def queueSize(self):
         if self.isEmpty()==True:
@@ -69,18 +72,54 @@ class linearQueue():
         print("Size:",self.queueSize())
         print("isEmpty():",self.isEmpty())
         print("isFull():",self.isFull())
-        print("="*20)
+        return ("="*20)
 
+def end():
+    exit()
+
+def list_of_commands():
+    print('''List Of Commands:
+
+enqueue value ==> adds value to queue
+dequeue ==> removes item at the front of the queue, and returns its value
+isempty ==> returns if queue is full or not
+isfull ==> returns if queue is empty or not
+showqueue ==> returns queue
+size ==> returns current size of queue
+debug ==> returns information about queue and the array it is in
+end ==> ends the program
+commands ==> shows this list of commands''')
+    return ''
     
+print("\nQUEUES v1, Type 'commands' For List Of Commands")
 
-test = linearQueue(5)
-test.enQueue("One")
-test.enQueue("2")
-test.deQueue()
-test.deQueue()
-test.enQueue("1")
-test.enQueue("2")
-test.debugQueue()
-print(test.queue)
-test.showQueue()
+queue1 = linearQueue(int(input("\nInitialise Queue Length:")))
+
+commands_dic = {
+    "enqueue": queue1.enQueue,
+    "dequeue": queue1.deQueue,
+    "isempty": queue1.isEmpty,
+    "isfull": queue1.isFull,
+    "showqueue": queue1.showQueue,
+    "size": queue1.queueSize,
+    "debug": queue1.debugQueue,
+    "end": end,
+    "commands":list_of_commands,
+    }
+
+valid = True
+while valid:
+    try:
+        user_input = input("\n>")
+        print("")
+        user_input = user_input.split()
+        command = user_input[0]
+        if len(user_input)>1:
+            value = user_input[1]
+            commands_dic[command](value)
+        else:
+            print(commands_dic[command]())
+    except:
+        print("Invalid Input.")
+        
 
