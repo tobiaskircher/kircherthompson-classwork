dd7e3daeb9df6131c5783c1bda78cc0cd36a646a
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2020-11-04 10:45:25 +0000

    Create oop_worksheet1.py

---
+++ OOP/oop_worksheet1.py
@@ -0,0 +1,32 @@
+class Dog:
+    def __init__(self, myName,myColour):
+        self.__name = myName
+        self.__colour = myColour
+    #end procedure
+
+    def bark(self, barkTimes):
+        for i in range(barkTimes):
+            print("Woof!")
+        #next i
+    #end procedure
+
+    def get_colour(self):
+        return self.__colour
+    #end procedure
+
+    def set_colour(self, newColour):
+        self.__colour = newColour
+    #end procedure
+
+    def get_name(self):
+        return self.__name
+    #end procedure
+    
+#end class
+
+dog1 = Dog("Harry","White")
+print(dog1._Dog__name) #accessing hidden variables
+print(dog1.get_name())
+print(dog1.get_colour())
+dog1.bark(1)
+
