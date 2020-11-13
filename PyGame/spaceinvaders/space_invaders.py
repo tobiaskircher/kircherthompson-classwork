13e861e62ce7ef2ede8811336b4a553c6cb6fd2e
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2020-11-13 18:14:26 +0000

    space invaders

--- PyGame/spaceinvaders/space_invaders.py
+++ PyGame/spaceinvaders/space_invaders.py
@@ -9,7 +9,7 @@ BLACK = (0,0,0)
 WHITE = (255,255,255)
 BLUE = (0,0,255)
 YELLOW = (255,255,0)
-
+RED = (255,0,0)
 
 
 # -- Initialise PyGame
@@ -24,16 +24,13 @@ pygame.display.set_caption("Space Invaders")
 
 ## -- Define the class invader which is a sprite
 class Invader(pygame.sprite.Sprite):
-    # Define the constructor for snow
+    # Define the constructor for invader
     def __init__(self, color, width, height, speed):
         # Call the sprite constructor
         super().__init__()
         # Create a sprite and fill it with colour
         self.image = pygame.Surface([width,height])
-        #self.image.fill(color)
-        self.image = pygame.image.load("C:\spaceinvader1.png").convert()
-        # Set our transparent color
-        self.image.set_colorkey(WHITE)
+        self.image.fill(color)
         # Set the position of the sprite
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, 600)
@@ -48,7 +45,7 @@ class Invader(pygame.sprite.Sprite):
 #End Class
 
 class Player(pygame.sprite.Sprite):
-    # Define the constructor for snow
+    # Define the constructor for player
     def __init__(self, color, width, height):
         # Call the sprite constructor
         super().__init__()
@@ -63,7 +60,14 @@ class Player(pygame.sprite.Sprite):
         self.speed = 0
         #set lives
         self.lives = 5
+        #bullets
+        self.bullet_count = 50
+        #score
+        self.score = 0
     #End Procedure
+
+    def bullet_fired(self):
+        self.bullet_count -= 1
 
     def player_set_speed(self,val):
         self.speed = val
@@ -72,20 +76,40 @@ class Player(pygame.sprite.Sprite):
         if 0 <=(self.rect.x + self.speed)<= (size[0]-10):
             self.rect.x = self.rect.x + self.speed
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x_coordinate, y_coordinate):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.Surface([2,2])
+        self.image.fill(RED)
+        # Set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = x_coordinate 
+        self.rect.y = y_coordinate
+        # Set speed
+        self.speed = 2
+
+    def update(self):
+        self.rect.y -= self.speed
+        
+#endclass
+    
 # -- Exit game flag set to false
 done = False
 
-# Create a list of the snow blocks
+# Create groups
 invader_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
 # Create a list of all sprites
 all_sprites_group = pygame.sprite.Group()
 
 
-# Create the snow
+# Create the invaders
 number_of_invaders = 10 # we are creating 50 invaders
 for x in range(number_of_invaders):
-    my_invader = Invader(BLUE, 10, 10, 1) # snowflakes are white with size 5 by 5 px
-    invader_group.add(my_invader) # adds the new snowflake to the group of snowflakes
+    my_invader = Invader(BLUE, 10, 10, 1) # invaders are blue with size 10 by 10 px
+    invader_group.add(my_invader) # adds the new invader to the group of invaders
     all_sprites_group.add(my_invader) # adds it to the group of all Sprites
 #Next x
 
@@ -107,27 +131,45 @@ while not done:
                 player.player_set_speed(-3) # speed set to -3
             elif event.key == pygame.K_RIGHT: # - if the right key pressed
                 player.player_set_speed(3) # speed set to 3
+                
+            elif event.key == pygame.K_UP: # - if the up key pressed
+                if player.bullet_count > 0:
+                    player.bullet_fired() # remove 1 from bullet count
+                    new_bullet = Bullet((player.rect.x + 5),player.rect.y)
+                    bullet_group.add(new_bullet)
+                    all_sprites_group.add(new_bullet)
+                
         elif event.type == pygame.KEYUP: # - a key released
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player.player_set_speed(0) # speed set to 0
 
     # -- Game logic goes after this comment
     
-    # -- when invader hits the player add 5 to score.
+    # -- remove life when player is hit
     player_hit_group = pygame.sprite.spritecollide(player, invader_group, True)
+    for bullet in bullet_group:
+        bullet_hit_group = pygame.sprite.spritecollide(bullet,invader_group,True)
+        for foo in bullet_hit_group:
+            player.score+=5
+        
     all_sprites_group.update()
 
     for foo in player_hit_group:
         player.lives -= 1
+
         
     # -- Screen background is BLACK
     screen.fill (BLACK)
     all_sprites_group.draw(screen)
 
     # Select the font to use, size, bold, italics
-    font = pygame.font.SysFont('Calibri', 15, True, False)
-    scoretext = font.render("Lives:"+str(player.lives),True,WHITE)
-    screen.blit(scoretext, (0, 0))
+    font = pygame.font.SysFont('Calibri', 20, True, False)
+    livestext = font.render("Lives: "+str(player.lives),True,WHITE)
+    screen.blit(livestext, (0, 0))
+    scoretext = font.render("Score: "+str(player.score),True,WHITE)
+    screen.blit(scoretext, (0, 20))
+    bullettext = font.render("Bullets: "+str(player.bullet_count),True,WHITE)
+    screen.blit(bullettext, (0, 40))
     
     # -- flip display to reveal new position of objects
     pygame.display.flip()
