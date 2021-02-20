64b8d36d7e95217db7a4eaae92d06380a26a4962
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2021-02-20 17:57:55 +0000

    Draw rectangle in frame.
    https://docs.opencv.org/master/dc/da5/tutorial_py_drawing_functions.html
    Draw simple rectangle in frame

--- Project/cubesolver.py
+++ Project/cubesolver.py
@@ -1,15 +1,18 @@
 import cv2 as cv
 
+WHITE = (255,255,255)
 capture = cv.VideoCapture(0)
 
 if not capture.isOpened():
     print("Unable To Open Webcam.")
+    exit()
 
 while True:
     ret, frame = capture.read()
+
+    cv.rectangle(frame,(0,0),(100,100), WHITE,1)
     
     cv.imshow("Cube Solver", frame)
-    #Cannot add an icon
 
     if cv.waitKey(1) == 27:
         break
