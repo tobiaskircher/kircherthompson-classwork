524fb7a5b01f7d9f92ac4065c4f9230717f2b4a9
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2021-02-21 12:14:42 +0000

    Overlap fixed, changes to drawGrid()
    There was an overlap between orange and red, for now it has been fixed by changing their green values. drawgrid function has been renamed to something more appropriate and i am working on an output for the scan now

--- Project/cubesolver.py
+++ Project/cubesolver.py
@@ -18,16 +18,16 @@ else:
 
 #Colour Ranges
 #colour = [[r_min,r_max],[g_min,g_max],[b_min,b_max], "colour_name"]
-blue = [[150,220],[145,185],[65,110],"blue"]
-orange = [[0,115],[90,165],[205,255],"orange"]
-green = [[75,149],[170,200],[85,135],"green"]
-red = [[30,155],[45,130],[195,255],"red"]
-white = [[125,220],[145,220],[145,220],"white"]
-yellow = [[30,130],[170,215],[160,200],"yellow"]
+blue = [[150,220],[145,185],[65,110],"b"]
+orange = [[0,115],[110,165],[205,255],"o"]
+green = [[75,149],[170,200],[85,135],"g"]
+red = [[30,155],[45,109],[195,255],"r"]
+white = [[125,220],[145,220],[145,220],"w"]
+yellow = [[30,130],[170,215],[160,200],"y"]
 colours = [blue, orange, green, red, white, yellow]
     
 def getColour(rgb_value):
-    return_value = "none"
+    return_value = "?"
     for colour in colours:
         if colour[0][0] <= rgb_value[0] <= colour[0][1]:
             if colour[1][0] <= rgb_value[1] <= colour[1][1]:
@@ -37,7 +37,8 @@ def getColour(rgb_value):
     return return_value
 
 
-def drawGrid(frame):
+def scanFace(frame):
+    face = ''
     rectWidth = 50
     halfRectWidth = rectWidth // 2
     gap = 30
@@ -59,13 +60,14 @@ def drawGrid(frame):
 
             rectColour = PUREBLACK
             get_colour = getColour(rgb)
+            face += get_colour
             
-            if get_colour != "none":
+            if get_colour != "?":
                 rectColour = rgb
 
             cv.rectangle(frame,(x,y),(x+rectWidth,y+rectWidth), rectColour,1)
-            if x == columns[1]:
-                cv.putText(frame,''.join(str(rgb)),(x,y), font, 0.5,PUREWHITE,cv.LINE_4)
+            
+            cv.putText(frame,get_colour,(x,y), font, 1,PUREWHITE,cv.LINE_4)
 
     
 while True:
@@ -73,7 +75,7 @@ while True:
 
     frame = cv.flip(frame,1)
     
-    drawGrid(frame)
+    scanFace(frame)
     
     cv.imshow("Cube Solver", frame)
 
