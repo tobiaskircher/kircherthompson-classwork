b145ffc8de603f97dcfd24978d123bac3e42fc0e
Tobias K <55653566+tobiaskircher@users.noreply.github.com>
2021-04-28 20:03:47 +0100

    Update bubble and insertion sort.py

--- Sorts/bubble and insertion sort.py
+++ Sorts/bubble and insertion sort.py
@@ -14,12 +14,16 @@ def bubble_sort(bubble_list):
                 bubble_list[count] = temp2
                 bubble_list[count + 1] = temp1
                 no_swaps = False
+            #end if
             count += 1
+            #next i
+        #end for
+    #end while
     time_taken = round(time.time() - time_start, 3) 
 
     print(bubble_list)
     print(str(time_taken) + " seconds.")
-    
+#end function    
 
 
 def insertion_sort(insertion_list):
@@ -31,16 +35,23 @@ def insertion_sort(insertion_list):
         while item < insertion_list[previous_index] and previous_index >= 0:
             insertion_list[previous_index + 1] = insertion_list[previous_index]
             previous_index -= 1
+        #end while
         insertion_list[previous_index + 1] = item
+        #next i
+    #end for
+        
     time_taken = round(time.time() - time_start, 3)
     print(insertion_list)
     print(str(time_taken) + " seconds.")
-    
+#end function   
     
 
 the_list = []
-for i in range(2000):
+for i in range(10000):
     the_list.append(random.randint(0,1000))
+
+    #next i
+#end for
                     
 print(the_list)
 bubble_list = the_list[:]
